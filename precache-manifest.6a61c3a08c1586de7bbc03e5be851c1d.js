87ae09d687415b282936212aee34911e4d9ff8a1
ary108 <aryanpalave1@gmail.com>
2023-03-22 15:25:11 -0700

    Updates

---
+++ precache-manifest.6a61c3a08c1586de7bbc03e5be851c1d.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fc88f69a8d2e72bee4793329192ea5",
+    "url": "/bhaktibeats3/index.html"
+  },
+  {
+    "revision": "d7fdc62a5bc71cc6b907",
+    "url": "/bhaktibeats3/static/css/main.5fd35561.chunk.css"
+  },
+  {
+    "revision": "85f26c597e770650075a",
+    "url": "/bhaktibeats3/static/js/2.9cb59501.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bhaktibeats3/static/js/2.9cb59501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7fdc62a5bc71cc6b907",
+    "url": "/bhaktibeats3/static/js/main.96ef61aa.chunk.js"
+  },
+  {
+    "revision": "eed5c057d1a68865e56d",
+    "url": "/bhaktibeats3/static/js/runtime-main.724e313a.js"
+  },
+  {
+    "revision": "7d926b068cb7301ab2e179cf27f56747",
+    "url": "/bhaktibeats3/static/media/accelya.7d926b06.svg"
+  },
+  {
+    "revision": "4acb5f4b4ffcde879c29d048bea2d07d",
+    "url": "/bhaktibeats3/static/media/airbus.4acb5f4b.svg"
+  },
+  {
+    "revision": "f194156b2a440d593b48b04019296a69",
+    "url": "/bhaktibeats3/static/media/andrewng.f194156b.png"
+  },
+  {
+    "revision": "beb5fe2d95f1240e81c4b416c74bd886",
+    "url": "/bhaktibeats3/static/media/axa.beb5fe2d.svg"
+  },
+  {
+    "revision": "a4bb8cd4bf324d483650ffbf3e226cc1",
+    "url": "/bhaktibeats3/static/media/axisbank.a4bb8cd4.svg"
+  },
+  {
+    "revision": "0ebb670991f4d3e8a364aa622939faa8",
+    "url": "/bhaktibeats3/static/media/bhaktibeatslogo.0ebb6709.png"
+  },
+  {
+    "revision": "022693208be48db1a5e717e371813146",
+    "url": "/bhaktibeats3/static/media/blob.02269320.png"
+  },
+  {
+    "revision": "5d01125b14df3917baac0558da7fa146",
+    "url": "/bhaktibeats3/static/media/certificates5.5d01125b.png"
+  },
+  {
+    "revision": "1cfd5dba4a9210bcf77f5dbe48ec2e66",
+    "url": "/bhaktibeats3/static/media/download_on_the_app_store_badge_en.1cfd5dba.svg"
+  },
+  {
+    "revision": "232e0ca536a79e305072aa6c18398a82",
+    "url": "/bhaktibeats3/static/media/julio.232e0ca5.png"
+  },
+  {
+    "revision": "3b8f25db054e649be12f5d45bfbca4fd",
+    "url": "/bhaktibeats3/static/media/kara.3b8f25db.png"
+  },
+  {
+    "revision": "5697cf62a377919a8b8d9ba834a2667f",
+    "url": "/bhaktibeats3/static/media/loreal.5697cf62.svg"
+  },
+  {
+    "revision": "3910aa949db567b4129c5f9f08e80e17",
+    "url": "/bhaktibeats3/static/media/mirela-decorative.3910aa94.png"
+  },
+  {
+    "revision": "8ac3d472101d80ffaca4ecbdd73b434f",
+    "url": "/bhaktibeats3/static/media/novartis.8ac3d472.svg"
+  },
+  {
+    "revision": "639b0b9c32a5f431a37d6a81b5b34858",
+    "url": "/bhaktibeats3/static/media/pandg.639b0b9c.svg"
+  },
+  {
+    "revision": "81cc2007ed8418b9b281a7add10f8979",
+    "url": "/bhaktibeats3/static/media/playstore.81cc2007.png"
+  },
+  {
+    "revision": "ea2796d4a9aea703cea113140ab97ff9",
+    "url": "/bhaktibeats3/static/media/promoStat2.ea2796d4.png"
+  },
+  {
+    "revision": "e250c86360fe6bd80c42efdfa9ada277",
+    "url": "/bhaktibeats3/static/media/quizCompletion2.e250c863.png"
+  },
+  {
+    "revision": "2c7249d1d4281d198390ed0759d55fce",
+    "url": "/bhaktibeats3/static/media/robot-girl.2c7249d1.jpg"
+  },
+  {
+    "revision": "cd89f0bc3b49bbd7073672eaa67a0853",
+    "url": "/bhaktibeats3/static/media/sabrina.cd89f0bc.png"
+  },
+  {
+    "revision": "963402f90cada7deac4ca1ccea69f26d",
+    "url": "/bhaktibeats3/static/media/secondary-consumer-hero-img.963402f9.png"
+  },
+  {
+    "revision": "2317d7cdf19620a870735157c48163c8",
+    "url": "/bhaktibeats3/static/media/sk.2317d7cd.svg"
+  },
+  {
+    "revision": "1720b6e553dde635b5c81eaa8d5fee07",
+    "url": "/bhaktibeats3/static/media/tata.1720b6e5.svg"
+  }
+]);
